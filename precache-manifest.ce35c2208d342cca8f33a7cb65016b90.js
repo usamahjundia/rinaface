0fdff7b192d6b7ebf17576f333f1fb549e000b63
Jundi <usamahjundia@hotmail.com>
2019-10-31 13:50:21 +0700

    Updates

---
+++ precache-manifest.ce35c2208d342cca8f33a7cb65016b90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a89d18356abd8a688dc484ea4e7a2d",
+    "url": "/rinaface/index.html"
+  },
+  {
+    "revision": "3ecf0ae9508225c64a1e",
+    "url": "/rinaface/static/css/main.67955dc0.chunk.css"
+  },
+  {
+    "revision": "2928883d6a8fc4ada936",
+    "url": "/rinaface/static/js/2.49239468.chunk.js"
+  },
+  {
+    "revision": "3ecf0ae9508225c64a1e",
+    "url": "/rinaface/static/js/main.93ddb1d1.chunk.js"
+  },
+  {
+    "revision": "a450a09f71376e9cce3c",
+    "url": "/rinaface/static/js/runtime-main.9b938efe.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/rinaface/static/media/logo.25bf045c.svg"
+  }
+]);
